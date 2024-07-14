3ca1bce3a1b9537e84d87c5a8c4b29be2c73e219
y9Kap <wotblitz6allima4ok@gmail.com>
2024-07-14 22:21:09 +0300

    feat(master): fuckery x100 with gh actions and maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
     id("io.ktor.plugin") version "2.3.12"
-    id("maven-publish")
     id("signing")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "org.example"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
@@ -17,6 +17,36 @@ repositories {
 
 tasks.named<ShadowJar>("shadowJar") {
     enabled = false
+}
+
+mavenPublishing {
+    coordinates("com.example.Lesta-BLITZ-API", "Lesta-BLITZ-API", "0.2.0-SNAPSHOT")
+
+    pom {
+        name.set("Lesta-BLITZ-API")
+        description.set("Library for interacting with the Lesta Games API")
+        inceptionYear.set("2024")
+        url.set("https://github.com/y9Kap/Lesta-BLITZ-API")
+        licenses {
+            license {
+                name.set("The MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("y9Kap")
+                name.set("Ivan Korolev")
+                url.set("https://github.com/y9Kap/")
+            }
+        }
+        scm {
+            url.set("https://github.com/y9Kap/Lesta-BLITZ-API/")
+            connection.set("scm:git:git://github.com/y9Kap/Lesta-BLITZ-API.git")
+            developerConnection.set("scm:git:ssh://git@github.com/y9Kap/Lesta-BLITZ-API.git")
+        }
+    }
 }
 
 kotlin {
