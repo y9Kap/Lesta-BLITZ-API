6b753eadc36c5b2d5057080fc82aef1d258c3abc
y9Kap <wotblitz6allima4ok@gmail.com>
2024-07-14 15:57:58 +0300

    feat(master): fuckery x10 with gh actions and maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,16 @@ plugins {
     kotlin("plugin.serialization") version "2.0.0"
     id("io.ktor.plugin") version "2.3.12"
     id("maven-publish")
+    id("signing")
 }
 
 group = "org.example"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
+
 tasks.named<ShadowJar>("shadowJar") {
     enabled = false
 }
